d5cfc96810d118b8d38f93cbd4e1c891a578e8ab
Patrik Szkandera <patrik@laposa.ie>
2025-02-17 15:04:29 +0100

    fix api build error

--- editor-api/src/app.module.ts
+++ editor-api/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { join } from 'path';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { ConvertModule } from './convert/convert.module';
@@ -10,10 +8,6 @@ import appConfig from './app.config';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [appConfig] }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
-
     CommonModule,
     ConvertModule,
   ],

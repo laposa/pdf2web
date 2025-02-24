f3a6d130140a656d82590e5dd835844ba869d2ab
Norbert Laposa <norbert@laposa.ie>
2025-02-24 11:07:10 +0000

    remove React viewer app and editor

--- editor-app/src/components/ui/empty-state.tsx
+++
@@ -1,7 +0,0 @@
-type EmptyStateProps = {
-  text: string;
-};
-
-export const EmptyState = (props: EmptyStateProps) => {
-  return <>{props.text}</>;
-};
--- editor-app/src/components/ui/loader.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from "lucide-react";
-
-export const Loader = () => {
-  return (
-    <div className="flex justify-center">
-      <Loader2 className="w-6 h-6 text-muted-foreground animate-spin" />
-    </div>
-  );
-};
